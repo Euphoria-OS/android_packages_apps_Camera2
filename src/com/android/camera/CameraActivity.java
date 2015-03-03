4698dc6dfde6111f2bc7fde019a2fd6ca32d26b0
Danny Baumann <dannybaumann@web.de>
2015-03-03 10:44:52 +0100

    Introduce a 'device plugin' concept.
    It allows device specific code to work with the camera while it's
    active.
    
    Change-Id: Iec78bf4d632c71f02e830eb2ac05504370bba660

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -74,6 +74,7 @@ import com.android.camera.app.CameraAppUI;
 import com.android.camera.app.CameraController;
 import com.android.camera.app.CameraProvider;
 import com.android.camera.app.CameraServices;
+import com.android.camera.app.DevicePluginImpl;
 import com.android.camera.app.LocationManager;
 import com.android.camera.app.MemoryManager;
 import com.android.camera.app.MemoryQuery;
@@ -210,6 +211,7 @@ public class CameraActivity extends QuickActivity
     private int mCurrentModeIndex;
     private CameraModule mCurrentModule;
     private ModuleManagerImpl mModuleManager;
+    private DevicePluginImpl mDevicePlugin;
     private FrameLayout mAboveFilmstripControlLayout;
     private FilmstripController mFilmstripController;
     private boolean mFilmstripVisible;
@@ -525,6 +527,7 @@ public class CameraActivity extends QuickActivity
         }
         Log.v(TAG, "invoking onChangeCamera");
         mCameraAppUI.onChangeCamera();
+        mDevicePlugin.onCameraOpened(camera);
     }
 
     private void resetExposureCompensationToDefault(CameraAgent.CameraProxy camera) {
@@ -1405,6 +1408,9 @@ public class CameraActivity extends QuickActivity
         GcamHelper.init(getContentResolver());
         ModulesInfo.setupModules(mAppContext, mModuleManager);
 
+        mDevicePlugin = new DevicePluginImpl();
+        mDevicePlugin.onCreate(mAppContext);
+
         mSettingsManager = getServices().getSettingsManager();
         mSettingsManager.addListener(this);
         initPowerShutter();
@@ -1939,6 +1945,7 @@ public class CameraActivity extends QuickActivity
         getContentResolver().unregisterContentObserver(mLocalVideosObserver);
         getServices().getCaptureSessionManager().removeSessionListener(mSessionListener);
         mCameraAppUI.onDestroy();
+        mDevicePlugin.onDestroy();
         mModeListView.setVisibilityChangedListener(null);
         mCameraController = null;
         mSettingsManager = null;
